694019669c7c2125008e6f97cd2cae19a41b85bf
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-04-02 14:43:57 +0700

    first commit index.html and index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function addition() {
+  var number1 = document.getElementById("number_1").value;
+  var number2 = document.getElementById("number_2").value;
+
+  return result = Number(number1) + Number(number2);
+}
+
+function substract() {
+  var number1 = document.getElementById("number_3").value;
+  var number2 = document.getElementById("number_4").value;
+
+  return result = Number(number1) - Number(number2);
+}
+
+function multiple() {
+  var number1 = document.getElementById("number_5").value;
+  var number2 = document.getElementById("number_6").value;
+
+  return result = Number(number1) * Number(number2);
+}
+
+function divide() {
+  var number1 = document.getElementById("number_7").value;
+  var number2 = document.getElementById("number_8").value;
+
+  return result = Number(number1) / Number(number2);
+}
