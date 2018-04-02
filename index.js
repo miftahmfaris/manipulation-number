c6a9388b80dcdedd43f37ca61782463bf020fc21
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-04-02 16:35:39 +0700

    Update result of calculation number

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ function addition() {
   var number1 = document.getElementById("number_1").value;
   var number2 = document.getElementById("number_2").value;
 
-  return result = Number(number1) + Number(number2);
+  return document.getElementById("addCalculation").value = Number(number1) + Number(number2);
 }
 
 function substract() {
   var number1 = document.getElementById("number_3").value;
   var number2 = document.getElementById("number_4").value;
 
-  return result = Number(number1) - Number(number2);
+  return document.getElementById("subCalculation").value = Number(number1) - Number(number2);
 }
 
 function multiple() {
   var number1 = document.getElementById("number_5").value;
   var number2 = document.getElementById("number_6").value;
 
-  return result = Number(number1) * Number(number2);
+  return document.getElementById("multipleCalculation").value = Number(number1) * Number(number2);
 }
 
 function divide() {
   var number1 = document.getElementById("number_7").value;
   var number2 = document.getElementById("number_8").value;
 
-  return result = Number(number1) / Number(number2);
+  return document.getElementById("divideCalculation").value = Number(number1) / Number(number2);
 }
