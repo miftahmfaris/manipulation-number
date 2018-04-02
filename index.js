e7a3853ac1942a01acb21c623e4c4c655d89a9de
Miftah Faris <miftah.faris07@yahoo.co.id>
2018-04-02 17:21:01 +0700

    Update display simple calculator

--- index.js
+++ index.js
@@ -2,26 +2,54 @@ function addition() {
   var number1 = document.getElementById("number_1").value;
   var number2 = document.getElementById("number_2").value;
 
-  return document.getElementById("addCalculation").value = Number(number1) + Number(number2);
+  document.getElementById("number_1").value = "";
+  document.getElementById("number_2").value = "";
+
+  return document.getElementById("calculationResult").value = Number(number1) + Number(number2);
 }
 
 function substract() {
-  var number1 = document.getElementById("number_3").value;
-  var number2 = document.getElementById("number_4").value;
+  var number1 = document.getElementById("number_1").value;
+  var number2 = document.getElementById("number_2").value;
 
-  return document.getElementById("subCalculation").value = Number(number1) - Number(number2);
+  document.getElementById("number_1").value = "";
+  document.getElementById("number_2").value = "";
+
+  return document.getElementById("calculationResult").value = Number(number1) - Number(number2);
 }
 
 function multiple() {
-  var number1 = document.getElementById("number_5").value;
-  var number2 = document.getElementById("number_6").value;
+  var number1 = document.getElementById("number_1").value;
+  var number2 = document.getElementById("number_2").value;
 
-  return document.getElementById("multipleCalculation").value = Number(number1) * Number(number2);
+  document.getElementById("number_1").value = "";
+  document.getElementById("number_2").value = "";
+
+  return document.getElementById("calculationResult").value = Number(number1) * Number(number2);
 }
 
 function divide() {
-  var number1 = document.getElementById("number_7").value;
-  var number2 = document.getElementById("number_8").value;
+  var number1 = document.getElementById("number_1").value;
+  var number2 = document.getElementById("number_2").value;
 
-  return document.getElementById("divideCalculation").value = Number(number1) / Number(number2);
+  document.getElementById("number_1").value = "";
+  document.getElementById("number_2").value = "";
+
+  return document.getElementById("calculationResult").value = Number(number1) / Number(number2);
 }
+
+document.getElementById("addButton").addEventListener("click", function() {
+  document.getElementById("textResult").innerHTML = "Addition Result"
+})
+
+document.getElementById("subButton").addEventListener("click", function() {
+  document.getElementById("textResult").innerHTML = "Substract Result"
+})
+
+document.getElementById("multipleButton").addEventListener("click", function() {
+  document.getElementById("textResult").innerHTML = "Multiple Result"
+})
+
+document.getElementById("divideButton").addEventListener("click", function() {
+  document.getElementById("textResult").innerHTML = "Divide Result"
+})
